0101ba40fc25e93b9c18e638962cef320f693296
Fabian Vieri <42199171+fabianvieri@users.noreply.github.com>
2023-10-17 08:16:42 +0000

    new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { TextField, TextArea, Button } from "@radix-ui/themes";
+
+export default function Page() {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
+      <TextArea placeholder="Description" />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+}
