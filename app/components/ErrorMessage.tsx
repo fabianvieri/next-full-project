42185ca03f83a6509c76a120520f2664d8c1a403
Fabian Vieri <42199171+fabianvieri@users.noreply.github.com>
2023-10-20 08:11:31 +0000

    Update ErrorMessage.tsx

--- app/components/ErrorMessage.tsx
+++ app/components/ErrorMessage.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from "react";
 import { Text } from "@radix-ui/themes";
+
 export default function ErrorMessage({ children }: PropsWithChildren) {
   if (!children) return null;
   return (
